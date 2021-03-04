ad144a75f028cd2d9400e2e16fbccdf67f6d74c2
Edgar <edgar.mkhitaryan989@gmail.com>
2021-03-04 23:13:12 +0400

    keyPress

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+'use strict';
 const btn = document.querySelectorAll('.btn');
 const overlay = document.querySelector('.overlay');
 const module_1 = document.querySelector('.module_1');
 const close_btn = document.querySelectorAll('.close_btn');
 console.log(close_btn);
 function addModule() {
-    module_1.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+
+        module_1.classList.remove('hidden');
+        overlay.classList.remove('hidden');
 }
 
 function removeModule() {
@@ -13,8 +15,17 @@ function removeModule() {
     overlay.classList.add('hidden');
 }
 for(let i=0; i < btn.length; i++) {
+        
     btn[i].addEventListener('click', addModule);
 }
 
 for(let i=0; i < close_btn.length; i++) 
 close_btn[i].addEventListener('click', removeModule);
+
+
+window.addEventListener('keydown', (el) => {
+    if(el.code === 'Enter') {
+        removeModule();
+    }
+});
+overlay.addEventListener('click', removeModule);
